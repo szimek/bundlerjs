9712c8853422779eb266d52d19826f528d31e034
Szymon Nowak <szimek@gmail.com>
2016-08-17 23:17:29 +0200

    Switch from 'commander' to 'yargs'

--- bin/cli.js
+++ bin/cli.js
@@ -1,20 +1,46 @@
 #!/usr/bin/env node
-const commander = require('commander');
+const colors = require('colors');
+const yargs = require('yargs');
 const CLIEngine = require('../lib/cli-engine');
+const version = require('../package.json').version;
 
 // TODO use process.exit(<code>) on errors
 
-commander
-  .version('0.0.1')
-  .arguments('<command>')
-  .action((command) => {
-    switch (command) {
-      case 'check':
-        CLIEngine.check();
-        break;
-      case 'install':
-      default:
-        CLIEngine.install();
-    }
-  })
-  .parse(process.argv);
+yargs
+  .command(
+    'check',
+    // eslint-disable-next-line max-len
+    'Check if the dependencies listed in package.json are satisfied by currently installed packages',
+    {},
+    CLIEngine.check
+  );
+
+yargs
+  .command(
+    'install',
+    ' Install the dependencies specified in your package.json',
+    {},
+    CLIEngine.install
+  );
+
+yargs
+  .alias('h', 'help')
+  .usage('Usage: $0 <command>')
+  .help();
+
+yargs
+  .alias('v', 'version')
+  .version(version);
+
+// Calling yargs.argv resets list of registered commands, so this needs to be called before
+const availableCommands = yargs.getCommandInstance().getCommands().concat('help');
+const argv = yargs.argv;
+const commands = argv._;
+const command = commands[0];
+
+// Lame way of handling the default and invalid commands
+if (!commands.length) {
+  CLIEngine.install();
+} else if (availableCommands.indexOf(command) === -1 || commands.length > 1) {
+  console.log(colors.red(`Could not find command "${command}".`));
+}
